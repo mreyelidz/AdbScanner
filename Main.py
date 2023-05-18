fa696b4dc63e9b160bf531e5220955382a5eca03
BongHitz <itz.bonghitz@gmail.com>
2023-05-18 13:43:08 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
+import asyncio
 import subprocess
 import socket
-import os
 import struct
 import time
 
-# Define colors for outputs
 class colors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -13,73 +12,65 @@ class colors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-# Define a function to convert a string IP address to an unsigned int
-def convert_ip_to_uint(ip):
+async def convert_ip_to_uint(ip):
     return struct.unpack("!I", socket.inet_aton(ip))[0]
 
-# Define a function to convert an unsigned int IP address to a string
-def convert_uint_to_ip(num):
+async def convert_uint_to_ip(num):
     return socket.inet_ntoa(struct.pack('!I', num))
 
-# Define a function to scan a range of IP addresses
-def scan_ip_range(start_ip, end_ip, timeout=0.01):
-    start_uint = convert_ip_to_uint(start_ip)
-    end_uint = convert_ip_to_uint(end_ip)
-
-    active_ips = []
-    inactive_ips = []
-
+async def scan_ip_range(start_ip, end_ip, timeout=0.01):
+    start_uint = await convert_ip_to_uint(start_ip)
+    end_uint = await convert_ip_to_uint(end_ip)
+    active_ips = []; inactive_ips = []
     while start_uint <= end_uint:
-        current_ip = convert_uint_to_ip(start_uint)
+        current_ip = await convert_uint_to_ip(start_uint)
         try:
             socket.setdefaulttimeout(timeout)
             socket.socket().connect((current_ip, 80))
-            active_ips.append(current_ip)
+            active_ips.append(current_ip); print(f"{colors.GREEN}Scanned IP: {current_ip}{colors.ENDC}")
         except (socket.error, socket.timeout):
-            inactive_ips.append(current_ip)
+            inactive_ips.append(current_ip); print(f"{colors.FAIL}Scanned IP: {current_ip}{colors.ENDC}")
         start_uint += 1
     return active_ips, inactive_ips
 
-# Define the range of IP addresses to scan
-ip_range_start = "192.168.1.1"
-ip_range_end = "192.168.1.255"
-
-# Scan the IP address range
-start_time = time.time()
-active_ips, inactive_ips = scan_ip_range(ip_range_start, ip_range_end)
-elapsed_time = time.time() - start_time
-
-# Print the summary of IP scanning
-print(f"{colors.HEADER}IP Address Summary{colors.ENDC}")
-print(f"{colors.BLUE}Active IPs: {', '.join(active_ips)}{colors.ENDC}")
-print(f"{colors.FAIL}Inactive IPs: {', '.join(inactive_ips)}{colors.ENDC}")
-print(f"{colors.GREEN}Scanned {len(active_ips) + len(inactive_ips)} IPs in {elapsed_time:.2f} seconds.{colors.ENDC}")
-print("\n")
-
-# Set up the Android Device Bridge on each active device
-connected_ips = []
-unconnected_ips = []
-for ip in active_ips:
+async def adb_connect(ip):
     try:
-        subprocess.check_output(['adb', '-s', ip, 'tcpip', '5555'], stderr=subprocess.STDOUT, universal_newlines=True)
-        subprocess.check_output(['adb', 'connect', ip], stderr=subprocess.STDOUT, universal_newlines=True)
-        connected_ips.append(ip)
-    except subprocess.CalledProcessError as e:
-        unconnected_ips.append(ip)
-        print(f"{colors.WARNING}Error: {e.output}{colors.ENDC}")
+        await asyncio.create_subprocess_exec('adb', '-s', ip, 'tcpip', '5555', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        await asyncio.create_subprocess_exec('adb', 'connect', ip, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        return ip
+    except Exception as e:
+        print(f"{colors.WARNING}Error connecting to {ip}: {e}{colors.ENDC}")
+        return None
 
-# Print the summary of ADB connection
-print(f"{colors.HEADER}ADB Connection Summary{colors.ENDC}")
-print(f"{colors.GREEN}ADB Connected IPs: {', '.join(connected_ips)}{colors.ENDC}")
-print(f"{colors.WARNING}ADB Unconnected IPs: {', '.join(unconnected_ips)}{colors.ENDC}")
-print("\n")
+async def connect_ips(ips):
+    tasks = []
+    for ip in ips:
+        tasks.append(asyncio.create_task(adb_connect(ip)))
+    connected_ips = [ip for ip in await asyncio.gather(*tasks) if ip is not None]
+    for ip in connected_ips:
+        print(f"{colors.GREEN}ADB connected to {ip}.{colors.ENDC}")
+    return connected_ips
 
-# Write the list of connected IPs to a file
-try:
-    with open('connected_ips.txt', 'w') as file:
-        for ip in connected_ips:
-            file.write(ip + "\n")
-    # Print the confirmation of writing to file
-    print(f"{colors.BLUE}Successfully written the list of connected IPs to connected_ips.txt.{colors.ENDC}")
-except IOError as e:
-    print(f"{colors.WARNING}Error: {e}{colors.ENDC}")
+async def write_ips_to_file(connected_ips):
+    try:
+        with open('connected_ips.txt', 'w') as file:
+            for ip in connected_ips:
+                file.write(ip + "\n")
+        print(f"{colors.BLUE}List of connected IPs {' '.join(connected_ips)} written to connected_ips.txt.{colors.ENDC}")
+    except IOError as e:
+        print(f"{colors.WARNING}Error writing file: {e}{colors.ENDC}")
+
+async def main():
+    start_time = time.monotonic()
+    active_ips, inactive_ips = await scan_ip_range("108.192.0.0", "108.192.0.255")
+    print(f"{colors.BLUE}{len(active_ips)} IPs scanned in {time.monotonic() - start_time:.2f} seconds.{colors.ENDC}")
+
+    start_time = time.monotonic()
+    connected_ips = await connect_ips(active_ips)
+    print(f"{colors.GREEN}{len(connected_ips)} devices connected to ADB in {time.monotonic() - start_time:.2f} seconds.{colors.ENDC}")
+
+    start_time = time.monotonic()
+    await write_ips_to_file(connected_ips)
+    print(f"{colors.BLUE}List of connected IPs written to file in {time.monotonic() - start_time:.2f} seconds.{colors.ENDC}")
+
+asyncio.run(main())
